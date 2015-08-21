5b2ce7f9386f33b9259ac723b62c207502e5ac88
huangyeqin <huangyeqing@youku.com>
2015-08-21 16:55:19 +0800

    add ffplay test

--- sendUdpStream.sh
+++ sendUdpStream.sh
@@ -8,7 +8,8 @@
 #*****comment*********
 
 
-ffmpeg -i "/home/vi/Aging_Test_Video.mp4"  -vcodec libx264  -g 60 -vb 150000 -strict experimental -acodec aac -ab 96000 -ar 48000 -ac 2 -vbsf h264_mp4toannexb -f mpegts udp://192.168.11.224:10000
+ffmpeg -i "/home/vi/Aging_Test_Video.mp4"  -vcodec libx264  -g 60 -vb 150000 -strict experimental -acodec aac -ab 96000 -ar 48000 -ac 2 -vbsf h264_mp4toannexb -f mpegts udp://127.0.0.1:10000
 
 
-
+#ffmpeg -i /dev/video0  -vcodec libx264  -g 60 -vb 150000 -strict experimental -acodec aac -ab 96000 -ar 48000    -f mpegts udp://127.0.0.1:10000
+#ffmpeg -i "/home/vi/Aging_Test_Video.mp4"  -vcodec libx264  -g 60 -vb 150000 -strict experimental -acodec aac -ab 96000 -ar 48000    -f mpegts udp://127.0.0.1:10000
